1b574c1b242b04f0141b6dc9dd72505fb204adf2
Polishchuk <a.polishchuk@mobidev.biz>
2017-06-01 18:44:19 +0300

    - initialize app

---
+++ src/app/device-list/device-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeviceListComponent } from './device-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DeviceListComponent]
+})
+export class DeviceListModule { }
