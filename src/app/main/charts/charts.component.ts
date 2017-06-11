66c0c3f7731d1062da6dbbda1d29d43431f567d4
polischuka <a.polischuk@mobidev.biz>
2017-06-11 23:37:58 +0300

    - add dynamic charts for diferent sensors (need to fox for voc and souble sound)

--- src/app/main/charts/charts.component.ts
+++ src/app/main/charts/charts.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { HeaderService } from '../../shared/services/header/header.service';
+import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { BaseChartDirective } from 'ng2-charts/index';
+import { DeviceDataService } from '../../shared/services/device-data/device-data.service';
+import { IDeviceData } from '../../shared/services/device-data/device.interfaces';
 
 @Component({
 	selector: 'app-charts',
@@ -7,32 +10,87 @@ import { HeaderService } from '../../shared/services/header/header.service';
 	styleUrls: ['./charts.component.scss']
 })
 
-export class ChartsComponent {
+export class ChartsComponent implements OnInit, OnDestroy{
+	lastItemSub: Subscription;
+	lastListSub: Subscription;
+	lineChartData:Array<any> = [];
+	lineChartLabels:Array<any> = [];
+
 	selected: string = 'temperature';
+	displayLast: number = 20;
+	autoUpdate: boolean = true;
 
-	constructor() {}
 
+	@ViewChild(BaseChartDirective) chart: BaseChartDirective;
+	constructor(private deviceDataService: DeviceDataService) {}
 
-	lineChartData:Array<any> = [
-		{data: [65.3, 59.4, 80.8, 81.98, 56.45, 55.87, 40.05], label: 'Series A'}
-	];
-	lineChartLabels:Array<any> = ['1', '2', '3', '4', '5', '6', '7'];
-	lineChartType:string = 'line';
-	lineChartOptions:any = {
-		legend: {display: false},
-		responsive: true
-	};
+	ngOnInit(): void{
+		this.loadData();
+	}
+
+	ngOnDestroy(): void {
+		if(this.lastItemSub) this.lastItemSub.unsubscribe();
+	}
 
 	onSelect(sensorName: string): void{
-		console.log(sensorName);
+		this.selected = sensorName;
+		this.lastItemSub.unsubscribe();
+		this.lastListSub.unsubscribe();
+		this.loadData();
 	}
 
-	public chartClicked(e:any):void {
-		console.log(e);
+	onAutoUpdate({checked}: any): void {
+		this.autoUpdate = !this.autoUpdate;
+		if(!this.autoUpdate) this.lastItemSub.unsubscribe();
+		else this.ngOnInit();
 	}
 
-	public chartHovered(e:any):void {
-		console.log(e);
+	loadData(): void {
+		this.lastListSub = this.deviceDataService
+			.getLast(this.displayLast)
+			.subscribe(this.updateInitialArr.bind(this));
+
+		if(this.autoUpdate){
+			this.lastItemSub = this.deviceDataService.getLast()
+				.subscribe((data: Array<IDeviceData>) => {
+					if(data && data.length){
+						this.addPointToArr(data[0]);
+					}
+				});
+		}
 	}
 
+	updateInitialArr(data: Array<IDeviceData>): void {
+		this.lineChartLabels.splice(0);
+		this.lineChartData.splice(0);
+
+		data.forEach((item: IDeviceData) => {
+			this.lineChartLabels.push(this.getTimeString(item.timestamp));
+			this.lineChartData.push(item[this.selected]);
+		});
+
+		this.chart.chart.update();
+		this.lastListSub.unsubscribe();
+	}
+
+	addPointToArr(data: IDeviceData): void {
+		if(this.lineChartLabels.length){
+			let time: string = this.getTimeString(data.timestamp),
+				length: number = this.displayLast - 1;
+
+			if(this.lineChartLabels.length > length){
+				this.lineChartLabels.shift();
+				this.lineChartData.shift();
+			}
+
+			this.lineChartLabels.push(time);
+			this.lineChartData.push(data[this.selected]);
+			this.chart.chart.update();
+		}
+	}
+
+	getTimeString(timestamp: number): string {
+		let time: Date = new Date(timestamp);
+		return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+	}
 }
